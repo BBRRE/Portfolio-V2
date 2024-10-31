bc16578ce384ca0a78dc8d2e85834c088a0825ad
Bilaal Bare <Bilaalbarre@hotmail.com>
2024-10-31 23:32:03 +0000

    cc

---
+++ .history/src/App_20241031231322.jsx
@@ -0,0 +1,28 @@
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Archi from './Subjects/Archi.jsx'
+import CompSci from './Subjects/CompSci.jsx'
+import EEng from './Subjects/EEng.jsx'
+import Other from './Subjects/Other.jsx'
+import { HomePage } from './HomePage'
+
+function App() {
+
+  return (
+    <>
+      <Router>
+            <Routes>
+              <BrowserRouter basename={"/Portfolio-V2"}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/Architecture" element={<Archi />} />
+                <Route path="/Electronic Eng" element={<EEng />} />
+                <Route path="/Computer Sci" element={<CompSci />} />
+                <Route path="/Other" element={<Other />} />
+                </BrowserRouter>
+            </Routes>
+        </Router>
+    </>
+  )
+}
+
+export default App
---
+++ .history/src/App_20241031231438.jsx
@@ -0,0 +1,26 @@
+import { Router, Routes, Route } from 'react-router-dom';
+import Archi from './Subjects/Archi.jsx'
+import CompSci from './Subjects/CompSci.jsx'
+import EEng from './Subjects/EEng.jsx'
+import Other from './Subjects/Other.jsx'
+import { HomePage } from './HomePage'
+
+function App() {
+
+  return (
+    <>
+      <Router>
+            <Routes>
+              
+                <Route path="/" element={<HomePage />} />
+                <Route path="/Architecture" element={<Archi />} />
+                <Route path="/Electronic Eng" element={<EEng />} />
+                <Route path="/Computer Sci" element={<CompSci />} />
+                <Route path="/Other" element={<Other />} />
+            </Routes>
+        </Router>
+    </>
+  )
+}
+
+export default App
