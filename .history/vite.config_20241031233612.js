340e7a3a34ec43708e530c413025a615d0b498d6
Bilaal Bare <Bilaalbarre@hotmail.com>
2024-10-31 23:37:32 +0000

    bfg

--- .history/vite.config_20241031233612.js
+++ .history/vite.config_20241031233612.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './', // Change this from '/Portfolio-V2/' to './'
+  plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
