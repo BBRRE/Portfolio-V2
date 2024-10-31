8b2e0ead9c4b4d909e1413a12add4bebf1c691b9
Bilaal Bare <137724145+BBRRE@users.noreply.github.com>
2024-10-31 16:09:57 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './Portfolio-V2/',
+  base: 'https://bbrre.github.io/Portfolio-V2/',
   plugins: [react()],
   assetsInclude: ['**/*.JPEG'],
 })
