c94c5c7dbb819e8260578fe505c38c78cade218b
Bilaal Bare <Bilaalbarre@hotmail.com>
2024-11-14 17:57:04 +0000

    Added NPM release

--- .history/src/Subjects/CompSci_20241114175117.jsx
+++ .history/src/Subjects/CompSci_20241114175117.jsx
@@ -12,7 +12,7 @@ const CompSci = () => {
           <h2 className=" text-left text-xl md:text-2xl lg:text-3xl font-semibold text-gray-800">
             Travel Log
           </h2>
-        <p className="text-gray-700 text-sm md:text-base leading-relaxed max-w-[400px]">
+        <p className="text-gray-700 text-sm md:text-base leading-relaxed">
           Website to share you Travel Logs, whether its a day trip to a neighbouring city or a 3 week long vacations spanning countries. Built with react
           and Firebase. It is still in development but I have linked a working prototype <a href="https://journey-3930f.web.app/" className='underline text-gray-600'>journey-3930f.web.app</a>
         </p>
@@ -27,18 +27,6 @@ const CompSci = () => {
           </h2>
         <p className="text-gray-700 text-sm md:text-base leading-relaxed max-w-[400px]">
         Includes mathematical proof and a more intuitive simulation to explain the Monkey and Hunter Problem  <a href="https://bbrre.github.io/Monkey-Hunter-Problem/index.html" className='underline text-gray-600'>https://bbrre.github.io/Monkey-Hunter-Problem/index.html</a>
-        </p>
-        </div>
-      </div>
-
-      <div className='flex md:flex-row flex-col justify-center align-middle w-[85%] h-[500px] mx-auto mt-24 mb-8'>
-        <img src={tr} />
-        <div className="flex flex-col ml-4 gap-3 justify-center">
-          <h2 className=" text-left text-xl md:text-2xl lg:text-3xl font-semibold text-gray-800">
-            React Gallery Component
-          </h2>
-        <p className="text-gray-700 text-sm md:text-base leading-relaxed max-w-[400px]">
-        Decided to release a simple gallery component I built for a personal website, not finding one online that fit the circumstance. I am currently developing it further to make it more intelligent.<a href="https://www.npmjs.com/package/react-gallery-component2" className='underline text-gray-600'>https://www.npmjs.com/package/react-gallery-component2</a>
         </p>
         </div>
       </div>
