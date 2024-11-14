530b2d91eb5cf02af117856b662d08714e8e1272
Bilaal Bare <137724145+BBRRE@users.noreply.github.com>
2024-11-14 17:48:03 +0000

    Update HomePage.jsx

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -21,7 +21,7 @@ export const HomePage = () => {
             
             <p className='font-inconsolata text-lg md:text-2xl w-full md:w-[500px] md:ml-[150px] mt-4 md:mt-[40px] mb-6 z-10'>
               Aspiring Architect. <br/>
-              I'm interested in: Maths, Dcience and Web Development.
+              I'm interested in: Maths, Science and Web Development.
             </p>
             
             <div className='ml-4 md:ml-[150px] z-10'>
